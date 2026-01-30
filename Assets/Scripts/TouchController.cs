6e8ce064d2d0acda6e7f3fb4ae0ee24109c6264c
Wilber <wilber.sa@hotmail.com>
2026-01-30 20:18:27 -0300

    Scripts for the touch controller

---
+++ Assets/Scripts/TouchController.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class TouchController : MonoBehaviour
+{
+
+    public Vector2 mousePositionPrev;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButton(0));
+        {
+            // mousePosition NOW - mousePosition PREV = delta
+
+            Move(Input.mousePosition.x - mousePositionPrev.x);
+        }
+
+        mousePositionPrev = Input.mousePosition;
+    }
+
+    public void Move(float speed)
+    {
+        transform.position += Vector3.right * Time.deltaTime * speed;   
+    }
+}
