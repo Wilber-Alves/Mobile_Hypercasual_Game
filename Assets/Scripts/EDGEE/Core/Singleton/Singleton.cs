ea8b2c81478e9da8b871ecacc0f37ec46d08151d
Wilber <wilber.sa@hotmail.com>
2026-02-04 18:45:20 -0300

    Creating the EDGEE - CORE folder
    Creating the EDGEE - CORE folder - including a Singleton script

---
+++ Assets/Scripts/EDGEE/Core/Singleton/Singleton.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+namespace EDGEE.Core.Singleton
+{
+
+    public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+    {
+        public static T Instance;
+
+        private void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = GetComponent<T>();
+                DontDestroyOnLoad(gameObject);
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+
+    }
+}
