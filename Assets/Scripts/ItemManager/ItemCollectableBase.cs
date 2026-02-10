b9047cff554ed241774fade8bc2fd44bf2e996c1
Wilber <wilber.sa@hotmail.com>
2026-02-10 19:38:53 -0300

    Update ItemCollectableBase.cs

--- Assets/Scripts/ItemManager/ItemCollectableBase.cs
+++ Assets/Scripts/ItemManager/ItemCollectableBase.cs
@@ -1,25 +1,24 @@
 using UnityEngine;
-using System.Collections.Generic;
 using System.Collections;
+using System.Collections.Generic;
 
 public class ItemCollectableBase : MonoBehaviour
 {
-
     public string compareTag = "Player";
 
     [Header("Particles")]
-    public ParticleSystem particleSystem;
+    public ParticleSystem pSystem;
 
     [Header("Sounds")]
     public AudioSource audioSource;
 
     public float timeToHide = 0.5f;
-    public GameObject graphicItem; 
-
+    public GameObject graphicItem;
 
     private void Awake()
     {
-        if (particleSystem != null) particleSystem.transform.SetParent(null);
+        
+        if (pSystem != null) pSystem.transform.SetParent(null);
     }
 
     private void OnTriggerEnter(Collider collision)
@@ -27,41 +26,31 @@ public class ItemCollectableBase : MonoBehaviour
         if (collision.transform.CompareTag(compareTag))
         {
             Collect();
-            OnCollect();
         }
     }
 
     protected virtual void Collect()
     {
+        
         if (GetComponent<SpriteRenderer>() != null) GetComponent<SpriteRenderer>().enabled = false;
         if (GetComponent<Collider>() != null) GetComponent<Collider>().enabled = false;
 
         if (graphicItem != null) graphicItem.SetActive(false);
-        Invoke("HideObject", timeToHide);
 
-        OnCollect();
-    }
 
-    private void HideObject()
-    {
-        gameObject.SetActive(false);
-    }
-
-    protected virtual void OnCollect()
-    {
-        if (particleSystem != null) particleSystem.Play();
-
+        if (pSystem != null) pSystem.Play();
         if (audioSource != null && audioSource.clip != null)
         {
             audioSource.spatialBlend = 0f;
             audioSource.Play();
-
-            Destroy(gameObject, 0.5f);
-
         }
-        else
-        {
-            Destroy(gameObject);
-        }
+
+        OnCollect();
+    }
+
+    protected virtual void OnCollect()
+    {
+
+        Destroy(gameObject, timeToHide);
     }
 }
