e6ff7c554bfbf61061ae3f0cd2b6240b2e44cae9
Wilber <wilber.sa@hotmail.com>
2026-02-12 17:22:16 -0300

    Coin collection power-up script

---
+++ Assets/Scripts/PowerUP/PowerUpCoins.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+    public class PowerUpCoins : PowerUpBase
+    {
+        [Header("Coin Collector")]
+        public float sizeAmount = 7;
+        protected override void StartPowerUp()
+        {
+            base.StartPowerUp();
+            PlayerController.Instance.ChangeCoinCollectorSize(sizeAmount);
+        }
+        protected override void EndPowerUp()
+        {
+            base.EndPowerUp();
+            PlayerController.Instance.ChangeCoinCollectorSize(1);
+        }
+    }
