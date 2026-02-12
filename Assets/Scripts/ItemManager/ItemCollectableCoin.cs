056e7c9e26b93756ccc5b7a6fbc5725577b232c7
Wilber <wilber.sa@hotmail.com>
2026-02-12 17:13:25 -0300

    Update ItemCollectableCoin.cs

--- Assets/Scripts/ItemManager/ItemCollectableCoin.cs
+++ Assets/Scripts/ItemManager/ItemCollectableCoin.cs
@@ -5,9 +5,36 @@ using System.Collections;
 
 public class ItemCollectableCoin : ItemCollectableBase
 {
+    public new Collider collider;
+    public bool collect = false;
+    public float lerp = 5f;
+    public float minDistance = 1f;
+
+    private void Start()
+    {
+        //CoinsAnimationManager.Instance.RegisterCoin(this);
+    }
+    protected override void Collect()
+    {
+        OnCollect();
+    }
     protected override void OnCollect()
     {
         base.OnCollect();
-        ItemManager.Instance.AddCoins();
+        collider.enabled = false;
+        collect = true;
+        //PlayerController.Instance.Bounce();
+    }
+    private void Update()
+    {
+        if (collect)
+        {
+            transform.position = Vector3.Lerp(transform.position, PlayerController.Instance.transform.position, lerp * Time.deltaTime);
+            if (Vector3.Distance(transform.position, PlayerController.Instance.transform.position) < minDistance)
+            {
+                //HideItens();
+                Destroy(gameObject);
+            }
+        }
     }
 }
