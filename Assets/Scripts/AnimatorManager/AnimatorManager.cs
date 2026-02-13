4e0a0952b3a122c13a4daa6600af05a7edb4734c
Wilber <wilber.sa@hotmail.com>
2026-02-13 16:23:04 -0300

    Update AnimatorManager.cs

--- Assets/Scripts/AnimatorManager/AnimatorManager.cs
+++ Assets/Scripts/AnimatorManager/AnimatorManager.cs
@@ -14,13 +14,14 @@ public class AnimatorManager : MonoBehaviour
         Dead 
     }
 
-    public void Play(AnimationType type)
+    public void Play(AnimationType type, float currentSpeedFactor = 1.0f)
     {
         foreach (var animation in animatorSetup)
         {
             if (animation.type == type)
             {
                 animator.SetTrigger(animation.trigger);
+                animator.speed = animation.speed * currentSpeedFactor;
                 break;
             }
         }
@@ -47,6 +48,7 @@ public class AnimatorManager : MonoBehaviour
     {
         public AnimatorManager.AnimationType type;
         public string trigger;
+        public float speed = 1f;
 
     }
 }
