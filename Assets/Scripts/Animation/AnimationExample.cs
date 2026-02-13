42179857aa48833763877e7aa59fde176e4b2a2c
Wilber <wilber.sa@hotmail.com>
2026-02-13 01:22:03 -0300

    Create AnimationExample.cs

---
+++ Assets/Scripts/Animation/AnimationExample.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class AnimationExample : MonoBehaviour
+{
+    public new Animation animation;
+
+    public AnimationClip run;
+    public AnimationClip idle;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            PlayAnimation(run);
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            PlayAnimation(idle);
+        }
+    }
+    private void PlayAnimation(AnimationClip c)
+    {
+        //animation.clip = c;
+        animation.CrossFade(c.name);
+
+    }
+
+}
