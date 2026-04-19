a7c36fd96379964b79ad68063c6f77f94358158b
Wilber <wilber.sa@hotmail.com>
2026-04-19 02:36:02 -0300

    Update EDGEEUtil.cs

--- Assets/Scripts/Utils/EDGEEUtil.cs
+++ Assets/Scripts/Utils/EDGEEUtil.cs
@@ -3,9 +3,28 @@ using System.Collections;
 using System.Collections.Generic;
 using Random = UnityEngine.Random;
 using DG.Tweening;
+//using UnityEditor;
 
 public static class EDGEEUtil
 {
+#if UNITY_EDITOR
+    [UnityEditor.MenuItem("EDGEE/New_Sphere %#&s")]
+    public static void NewSphere()
+    {
+        GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        sphere.transform.position = Vector3.zero;
+        Debug.Log("New Sphere Created");
+    }
+
+    [UnityEditor.MenuItem("EDGEE/New_Cube %#&c")]
+    public static void NewCube()
+    {
+        GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        cube.transform.position = Vector3.zero;
+        Debug.Log("New Cube Created");
+    }
+#endif
+
     public static void Scale(this Transform t, float size = 1.2f)
     {
         t.localScale = Vector3.one * size;
